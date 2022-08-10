f07cbd10845375e480408bc5a366ace89cbd7921
Riki-lu <r0527618125@gmail.com>
2022-08-10 12:13:27 +0300

    correctionsUpload

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,12 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using static TestProject.Algorithm;
+using static TestProject.ClearQueryFromCustomerData;
+using static System.Console;
 class Program
 {
     static void Main()
     {
         string demoQuery = "Connections| where DestinationEntityType =~'AadIdentity'| extend a = a + b |let x= where a > 10";
-        var cleanQuery = MainFunction(demoQuery);
-        Console.WriteLine("Source query: "+demoQuery);
-        Console.WriteLine("New query: " + cleanQuery);
+        var cleanQuery = ReplaceCustomerDataInQuery(demoQuery);
+        if (cleanQuery.GetType() == typeof(string))
+        {
+            WriteLine("Source query: " + demoQuery);
+            WriteLine("New query: " + cleanQuery);
+        }
+        else
+        {
+            WriteLine("The query isn't valid. The errors:");
+            ((List<string>)cleanQuery).ForEach(x => WriteLine(x));
+        }
+
     }
 }
